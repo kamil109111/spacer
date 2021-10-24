99f2cb99553966a067700b58ed054ef5f27775e6
Kamil Lachowicz <kamil109111@gmail.com>
2021-10-24 03:12:08 +0200

    get im from nasa api

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,26 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+console.log("Hi, I'm Kamil - nice to meet you 🚀");
 
-/* place your code below */
+const search = document.querySelector(".search-value--js");
+const button = document.querySelector(".search-button--js");
 
-console.log('HELLO 🚀')
+
+button.addEventListener("click", () => {
+  if (search.value) {
+    console.log(search.value);
+    fetch("https://images-api.nasa.gov/search?q="+search.value+"&media_type=image")
+      .then((resp) => resp.json())
+      .then((resp) => {         
+        const container = document.querySelector('.resluts-list--js');
+        for (let item of resp.collection.items){
+          const results = item.links[0].href;      
+          const template = `<img class="search__img" src="${results}" />`
+          container.innerHTML += template;
+          }
+        })
+      .catch((error) => {
+        console.log("nie udało się pobrać");
+      });
+  }
+});
