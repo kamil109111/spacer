3ee50733cfa4efa4f529d9a1d442b01b3e555ae3
Kamil Lachowicz <kamil109111@gmail.com>
2021-10-24 10:33:58 +0200

    gallery style

--- src/js/index.js
+++ src/js/index.js
@@ -2,20 +2,30 @@ import "../scss/main.scss";
 
 console.log("Hi, I'm Kamil - nice to meet you 🚀");
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const search = document.querySelector(".search-value--js");
 const button = document.querySelector(".search-button--js");
+const container = document.querySelector('.resluts-list--js');
+
 
 
 button.addEventListener("click", () => {
+  container.innerHTML = "";
   if (search.value) {
     console.log(search.value);
     fetch("https://images-api.nasa.gov/search?q="+search.value+"&media_type=image")
       .then((resp) => resp.json())
-      .then((resp) => {         
-        const container = document.querySelector('.resluts-list--js');
+      .then((resp) => {        
         for (let item of resp.collection.items){
-          const results = item.links[0].href;      
-          const template = `<img class="search__img" src="${results}" />`
+          const results = item.links[0].href;
+          const width = getRandomInt(200,400);
+          const heigth = getRandomInt(200,400);      
+          const template = `<img class="search__img" style="width:${width}px; heigth:${heigth}px" src="${results}" />`
           container.innerHTML += template;
           }
         })
